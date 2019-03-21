7a412836819b69966d9c1e10ee18ad915fdec958
Jun Li <156735229@qq.com>
2019-03-21 19:27:44 +0800

    修改打包方式

--- base-start/src/main/java/com/lj/spring/start/StartApplication.java
+++ base-start/src/main/java/com/lj/spring/start/StartApplication.java
@@ -2,6 +2,8 @@ package com.lj.spring.start;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,12 +13,16 @@ import java.util.Date;
 @SpringBootApplication
 @RestController
 @RequestMapping("/")
-public class StartApplication {
+public class StartApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(StartApplication.class, args);
     }
-
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(StartApplication.class);
+    }
 
     @GetMapping
     public Object test() {
